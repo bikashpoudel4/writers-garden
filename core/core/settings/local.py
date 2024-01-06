781577f06bd0ff8692fc139102ce06ae6e5528b0
Bikash Poudel <bikashpoudel4@gmail.com>
2024-01-06 12:43:25 +0100

    Configured: Celery, Redis and Flower

--- core/core/settings/local.py
+++ core/core/settings/local.py
@@ -13,3 +13,10 @@ DEBUG = True
 # ALLOWED_HOSTS = []
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "saaras.soft@gmail.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Writers Garden"
