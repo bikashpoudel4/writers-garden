608733224b9d040a45dee295d30bb72e534cc95d
Bikash Poudel <bikashpoudel4@gmail.com>
2024-01-21 23:48:35 +0100

    clap functionality tested ok

--- core/core_apps/articles/urls.py
+++ core/core_apps/articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ArticleListCreateView, ArticleRetriveUpdateDestroyView
+from .views import ArticleListCreateView, ArticleRetriveUpdateDestroyView, ClapArticleView
 
 urlpatterns = [
     path("", ArticleListCreateView.as_view(), name="article-list-create"),
     path("<uuid:id>/", ArticleRetriveUpdateDestroyView.as_view(), name="article-retrieve-update-destroy"),
+    path("<uuid:article_id>/clap/", ClapArticleView.as_view(), name="clap-article"),
 ]
