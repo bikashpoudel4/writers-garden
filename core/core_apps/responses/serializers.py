0c4c0699c588aa74558ec143ca5bcb8dca2884ab
Bikash Poudel <bikashpoudel4@gmail.com>
2024-01-23 18:28:10 +0100

    response views models serializers

---
+++ core/core_apps/responses/serializers.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+
+from rest_framework import serializers
+from .models import Response
+
+class ResponseSerializer(serializers.ModelSerializer):
+    user_first_name = serializers.CharField(source="user.first_name", read_only=True)
+    article_title = serializers.CharField(sourde="article.title", read_only=True)
+
+    class Meta:
+        model = Response
+        fields = [
+            "id",
+            "user_first_name",
+            "article_title",
+            "parent_response",
+            "content",
+            "created_at",
+        ]
