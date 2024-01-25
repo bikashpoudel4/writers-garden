1cadeefdf1b8b8a05ed23b049b7682669e4662d3
Bikash Poudel <bikashpoudel4@gmail.com>
2024-01-25 15:30:57 +0100

    serializer test pass users

--- core/conftest.py
+++ core/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 from pytest_factoryboy import register
+from django.test import RequestFactory
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.contrib.auth.middleware import AuthenticationMiddleware
 
 from core_apps.users.tests.factories import UserFactory
 
@@ -16,3 +19,17 @@ def normal_user(db, user_factory):
 def super_user(db, user_factory):
     new_user = user_factory.create(is_superuser=True, is_staff=True)
     return new_user
+
+
+@pytest.fixture
+def mock_request():
+    factory = RequestFactory()
+    request = factory.get("/")
+    middleware = SessionMiddleware(lambda req: None)
+    middleware.process_request(request)
+    request.session.save()
+
+    auth_middleware = AuthenticationMiddleware(lambda req: None)
+    auth_middleware.process_request(request)
+
+    return request
